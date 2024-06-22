c2bec201c0ed029a0add2d6b0bfa13dca3ef525f
t0nn1x <antoonhrobust@gmail.com>
2024-06-22 18:51:27 +0200

    Add database migrations for feeds, Instagram sources, TikTok sources, and posts

---
+++ database/migrations/2024_06_21_200948_create_instagram_sources_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('instagram_sources', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('feed_id')->constrained('feeds')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('fan_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instagram_sources');
+    }
+};
